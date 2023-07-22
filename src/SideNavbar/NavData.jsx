784d5647159b2fd601a1aa4993c8c6d06256d757
Davinder Kumar <abhiverma97819@gmail.com>
2023-07-22 12:57:07 +0530

    Changes

--- src/SideNavbar/NavData.jsx
+++ src/SideNavbar/NavData.jsx
@@ -1,32 +1,23 @@
 export const NavData = [
-  // {
-  //   icon: 'search',
-  //   name: 'Home',
-  // },
   {
     icon: 'home-alt',
-    name: 'Home',
   },
   {
     icon: 'notepad',
-    name: 'Data',
   },
   {
     icon: 'detail',
-    name: 'detail',
   },
 ];
 
 export const BottomData = [
   {
     icon: 'bell',
-    name: 'Notification',
   },
   {
     icon: 'search',
   },
   {
     icon: 'log-in',
-    name: 'Logout',
   },
 ];
--- src/SideNavbar/SideNavbar.jsx
+++ src/SideNavbar/SideNavbar.jsx
@@ -29,83 +29,75 @@ const SideNavbar = () => {
 
   return (
     <>
-      {windowWidth < 800 ? (
-        <nav className="sidebar">
-          <div className="menu-bar">
-            <div className="menu">
-              <ul className="menu-links">
-                {NavData.map((data, key) => (
-                  <li className="nav-link" key={key}>
-                    <a href="#">
-                      <i className={`bx bx-${data.icon} icon`}></i>
-                    </a>
-                  </li>
-                ))}
-              </ul>
-            </div>
-            <div className="bottom-content">
-              <ul className="menu-links">
-                {BottomData.map(
-                  (data, key) =>
-                    data.icon !== 'bell' && (
-                      <li className="" key={key}>
-                        <a href="#">
-                          <i className={`bx bx-${data.icon} icon`}></i>
-                        </a>
-                      </li>
-                    )
-                )}
-              </ul>
-            </div>
-          </div>
-        </nav>
-      ) : (
-        <nav className={`sidebar ${showSearch ? 'close' : ''}`}>
-          <header>
-            <i
-              className="bx bx-chevron-right toggle ham"
-              onClick={handleToggleSidebar}
-            ></i>
-          </header>
-          <div className="menu-bar">
-            <div className="menu">
-              {windowWidth > 800 && (
-                <li className="search-box" onClick={handleSearchButtonClick}>
-                  <i className="bx bx-search icon"></i>
-                  <input type="text" placeholder="Search..." />
-                </li>
-              )}
-              <ul className="menu-links">
-                {NavData.map((data, key) => (
-                  <li className="nav-link" key={key}>
-                    <a href="#">
-                      <i className={`bx bx-${data.icon} icon`}></i>
-                      <span className="text nav-text">{data.name}</span>
-                    </a>
-                  </li>
-                ))}
-              </ul>
-            </div>
-            <div className="bottom-content">
-              <ul className="menu-links">
-                {BottomData.map(
-                  (data, key) =>
-                    data.icon !== 'search' && (
-                      <li className="" key={key}>
-                        <a href="#">
-                          <i className={`bx bx-${data.icon} icon`}></i>
-                          <span className="text nav-text">{data.name}</span>
-                        </a>
-                      </li>
-                    )
-                )}
-              </ul>
-            </div>
-          </div>
-        </nav>
-      )}
-
       <section className="home grid">
+        {windowWidth < 1000 ? (
+          <nav className="sidebar">
+            <div className="menu-bar">
+              <div className="menu">
+                <ul className="menu-links">
+                  {NavData.map((data, key) => (
+                    <li className="nav-link" key={key}>
+                      <a href="#">
+                        <i className={`bx bx-${data.icon} icon`}></i>
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <div className="bottom-content">
+                <ul className="menu-links">
+                  {BottomData.map(
+                    (data, key) =>
+                      data.icon !== 'bell' && (
+                        <li className="" key={key}>
+                          <a href="#">
+                            <i className={`bx bx-${data.icon} icon`}></i>
+                          </a>
+                        </li>
+                      )
+                  )}
+                </ul>
+              </div>
+            </div>
+          </nav>
+        ) : (
+          <nav className={`sidebar ${showSearch ? 'close' : ''}`}>
+            <div className="menu-bar">
+              <div className="menu">
+                {windowWidth > 800 && (
+                  <li className="search-box" onClick={handleSearchButtonClick}>
+                    <i className="bx bx-search icon"></i>
+                    <input type="text" placeholder="Search..." />
+                  </li>
+                )}
+                <ul className="menu-links">
+                  {NavData.map((data, key) => (
+                    <li className="nav-link" key={key}>
+                      <a href="#">
+                        <i className={`bx bx-${data.icon} icon`}></i>
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+              <div className="bottom-content">
+                <ul className="menu-links">
+                  {BottomData.map(
+                    (data, key) =>
+                      data.icon !== 'search' && (
+                        <li className="" key={key}>
+                          <a href="#">
+                            <i className={`bx bx-${data.icon} icon`}></i>
+                          </a>
+                        </li>
+                      )
+                  )}
+                </ul>
+              </div>
+            </div>
+          </nav>
+        )}
+
         <LeftSide BottomData={BottomData} windowWidth={windowWidth} />
         <MiddleSide />
         <RightSide />
